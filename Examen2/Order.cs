2a9971a642e3b2947246cc9c35abbb99b862caaf
Pohuș Tabita <emanuela.pohus@emanuel.ro>
2024-01-22 10:21:00 -1200

    Problema2

---
+++ Examen2/Order.cs
@@ -0,0 +1,15 @@
+﻿namespace Examen2
+{
+    internal class Order
+    {
+      
+            public string Customer { get; set; }
+            public DateTime Date { get; set; }
+            public decimal Total { get; set; }
+            abstract class PaymentSystem
+
+            {
+
+            }
+    }
+}
---
+++ Examen2/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Examen2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduceti numele clientului: ");
+            string customerName = Console.ReadLine();
+
+            Console.Write("Introduceti data comenzii :");
+            DateTime orderDate = DateTime.ParseExact(Console.ReadLine(), null);
+
+            Console.Write("Introduceti valoarea totala a comenzii: ");
+            decimal orderTotal = decimal.Parse(Console.ReadLine());
+
+            // Crearea obiectului de tip Order
+            Order order = new Order
+            {
+                Customer = customerName,
+                Date = orderDate,
+                Total = orderTotal;
+            }
+    }
+}
